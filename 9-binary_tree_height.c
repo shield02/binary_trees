03e709486334ab338d552df54b013e01137736e8
shield <myshield02@gmail.com>
2023-08-03 12:29:12 +0100

    code to find the height of a tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,19 +9,22 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	size_t left, right;
 
-	size_t left = 0, right = 0;
+	if (tree == NULL)
+		return (0);
 
 	if (tree->left)
 		left = 1 + binary_tree_height(tree->left);
+	else
+		left = 0;
 	if (tree->right)
 		right = 1 + binary_tree_height(tree->right);
+	else
+		right = 0;
 	if (left > right)
 		return (left);
 	else
 		return (right);
-	return (0);
 }
 
