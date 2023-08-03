955ac7886a7c8d5ca0b8fcda0d85262c312cb45e
shield <myshield02@gmail.com>
2023-08-03 11:34:42 +0100

    code to use inorder travese on a tree

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - use preorder travesal to go through tree
+ * binary_tree_inorder - use inorder travesal to go through tree
  * @tree: pointer to a root node
  * @func: pointer to a function
  *
  * Description: The function goes through a binary tree
- * using pre-order travesal
+ * using in-order travesal
  * Return: nothing
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if(tree && func)
+	if (tree && func)
 	{
 		binary_tree_inorder(tree->left, func);
 		func(tree->n);
